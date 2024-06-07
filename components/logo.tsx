0637b7b28cbf6233b68ab7fc45b0100cfd110d5a
Abdikarinmohamed <abdikarinmohamed826@gmail.com>
2024-06-06 22:30:09 -0500

    Add Marketting page or landing page of the project

---
+++ components/logo.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Link from 'next/link';
+import localFont from "next/font/local";
+import {cn} from "@/lib/utils";
+
+
+export const Logo = () => {
+    return (
+        <Link href="/">
+            <div className="hover:opacity-75 transition item-center gap-x-2 hidden md:flex">
+                <Image src="/logo.png" 
+                alt="Logo" 
+                width={30} 
+                height={30} />
+                <p className="text-lg text-neutral-700 pb-1">
+                    Taskify
+                </p>
+            </div>
+        </Link>
+
+    )}
